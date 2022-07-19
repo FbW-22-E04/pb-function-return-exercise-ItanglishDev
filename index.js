9912eb1fb9c3a17c1c4dd93903f062b2a25f88ea
chris <chrishethertonweb@gmail.com>
2022-07-19 14:45:44 +0200

    3 done

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ checkAge(19)
 // Write a function min(a,b) which returns the least of two numbers a and b.
 function min(a, b) {
   a < b ? console.log(a) : console.log(b);
+
 }
-min(3, -1
-)
+min(3, -1)
 
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
 function pow(x, n) {
-
+  console.log(x ** n);
 }
+pow(8, 3)
